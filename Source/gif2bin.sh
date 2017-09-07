a0f32f70675b296a0577e9c081532a869170e62e
DW0RKiN <ogion@centrum.cz>
2017-09-08 01:00:21 +0200

    Update gif2bin.sh

--- Source/gif2bin.sh
+++ Source/gif2bin.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-mkdir bin
+mkdir Bin
 rm ./grafika.bin
 rm ./grafika.tap
-rm ./bin/*
+rm ./Bin/*
 
-for soubor in ./gif/*.gif 
+for soubor in ./Gif/*.gif 
 do 
 	naco=${soubor##*/}
-	naco="./bin/${naco%.*}"  
+	naco="./Bin/${naco%.*}"  
 	./gif2bin ${soubor} ${naco} 
 done 
 
-ls ./bin -l 
-ls ./bin -l | awk 'BEGIN{ adr=24320; sum=1; printf("; SPRITY od adresy $%x = %i\n",adr,adr); } { if ($9!="") printf("%s\tequ\t$%x\t;%i %i. = %i\n",$9,adr,adr,sum++,$5); adr+=$5; } END{ print "; end sprites\t"adr; }' > ./sprites.h
+ls ./Bin -l 
+ls ./Bin -l | awk 'BEGIN{ adr=24320; sum=1; printf("; SPRITY od adresy $%x = %i\n",adr,adr); } { if ($9!="") printf("%s\tequ\t$%x\t;%i %i. = %i\n",$9,adr,adr,sum++,$5); adr+=$5; } END{ print "; end sprites\t"adr; }' > ./sprites.h
 
-cat ./bin/* >> ./grafika.bin 
+cat ./Bin/* >> ./grafika.bin 
 ./bin2tap grafika.bin
 
